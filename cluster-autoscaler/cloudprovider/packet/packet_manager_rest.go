7d1d8b3cb1541dbb6ed20c735956169de9580cc2
Bartłomiej Wróblewski <bwroblewski@google.com>
2020-11-27 17:33:05 +0000

    Update imports after scheduler scheduler/framework/v1alpha1 removal

--- cluster-autoscaler/cloudprovider/packet/packet_manager_rest.go
+++ cluster-autoscaler/cloudprovider/packet/packet_manager_rest.go
@@ -42,7 +42,7 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/utils/gpu"
 	"k8s.io/autoscaler/cluster-autoscaler/version"
 	klog "k8s.io/klog/v2"
-	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
+	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
 const (
